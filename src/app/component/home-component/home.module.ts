f012409f537bdf23fbde429b2863cd049838744e
Akmal <akmal@avancerpi.com>
2022-08-31 00:55:10 +0530

    done

--- src/app/component/home-component/home.module.ts
+++ src/app/component/home-component/home.module.ts
@@ -5,24 +5,27 @@ import { WeatherComponent } from './weather/weather.component';
 import { CurrancyComponent } from './currancy/currancy.component';
 import { TempratureComponent } from './temprature/temprature.component';
 import { RouterModule } from '@angular/router';
-import { routes } from './home-routing.module'
+import { routes } from './home-routing.module';
 import { SharedMaterialModule } from 'src/app/sharedmodule/shared-material.module';
 import { HomeComponent } from './home-component.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     WeatherComponent,
     CurrancyComponent,
     TempratureComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     SharedMaterialModule,
+    NgxSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  schemas: [
-  CUSTOM_ELEMENTS_SCHEMA
-],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeModule { }
+export class HomeModule {}
